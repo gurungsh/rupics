b9d62a3324729e13ce9ffcd9f94342cff68ca7e2
Shishir Gurung <gurungsh@gmail.com>
2019-10-02 11:42:16 -0500

    Updates

---
+++ precache-manifest.d4ee8f7d38a0a8ac96625cd3d7005929.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff03b525bdff712b9dc28ba4bc1ce727",
+    "url": "/rupics/index.html"
+  },
+  {
+    "revision": "980b83265826e582144b",
+    "url": "/rupics/static/css/main.f53a64dd.chunk.css"
+  },
+  {
+    "revision": "8a2d583d898acfce7e03",
+    "url": "/rupics/static/js/2.e984a1ee.chunk.js"
+  },
+  {
+    "revision": "980b83265826e582144b",
+    "url": "/rupics/static/js/main.17d48e1b.chunk.js"
+  },
+  {
+    "revision": "c0692ae899601737b4ba",
+    "url": "/rupics/static/js/runtime~main.1cdb0ba8.js"
+  }
+]);
